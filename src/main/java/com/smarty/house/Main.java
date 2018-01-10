5115d29814c86e89dfee502f4e1176a4f45b4028
nikolai.chmyr <nikolay.chmyr@igniteoutsourcing.com>
2018-01-10 15:28:59 +0200

    first commit

---
+++ src/main/java/com/smarty/house/Main.java
@@ -0,0 +1,16 @@
+package com.smarty.house;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by nik on 1/10/18.
+ */
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Main.class, args);
+    }
+}
